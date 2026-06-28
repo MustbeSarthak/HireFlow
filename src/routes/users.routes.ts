b21744667500c822717868bb8cb727469342e06b
sarthak <231179030+MustbeSarthak@users.noreply.github.com>
2026-06-28 23:29:37 +0530

    User Route - Not Finished

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import {loginUser, registerUser} from '../controllers/register.controller.js'
 import { authValidator } from '../middleware/auth.middleware.js';
+
 const router = express.Router();
 
 // router.get('/users', getAllUser);
@@ -8,8 +9,6 @@ const router = express.Router();
 
 //Register Routes
 router.post('/register', registerUser );
-
-
 
 // Login Routes
 router.get('/login', authValidator, loginUser);
